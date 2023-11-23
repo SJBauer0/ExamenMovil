3a3e7a0f5d1085d714bce4fda34667d54ae8ed72
sjimenezbauer <sjimenezbauer@gmail.com>
2023-11-23 10:41:33 -0600

    Initial Commit

---
+++ ExamenMovil/ExamenMovilApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenMovilApp.swift
+//  ExamenMovil
+//
+//  Created by sebastian Jimenez Bauer on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ExamenMovilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
