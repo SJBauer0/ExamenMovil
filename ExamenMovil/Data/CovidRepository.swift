ef1d8998b647ca3d28a3c57916888870ab62f680
sjimenezbauer <sjimenezbauer@gmail.com>
2023-11-23 11:50:47 -0600

    Added Repository and API Service

--- ExamenMovil/Data/CovidRepository.swift
+++ ExamenMovil/Data/CovidRepository.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+/**
+Creo la estrucutra del api
+**/
+
+struct Api {
+    static let base = "https://api.api-ninjas.com/v1/covid19?country="
+}
+
+/**
+Defino el protocolo
+**/
+
+protocol CovidAPIProtocol {
+    func getCovidList() async -> [Result]?
+}
+
+/**
+Implemtnacion del repositorio de NAME que
+utiliza el protocolo en el folder Domain
+**/
+
+class CovidRepository: CovidAPIProtocol {
+    
+    let nservice: CovidAPIService
+    
+    static let shared = CovidRepository()
+
+    init(nservice: CovidAPIService = CovidAPIService.shared) {
+        self.nservice = nservice
+    }
+
+    //Creo la funcion para obtener los resultados
+
+    func getCovidList() async -> [Result]? {
+        return await nservice.getCovidList()
+    }
+}
--- ExamenMovil/Data/Models/Network/CovidApiServices.swift
+++ ExamenMovil/Data/Models/Network/CovidApiServices.swift
@@ -8,3 +8,46 @@
 import Foundation
 
 // API Key: tmEwAP5zJ4bllnMGJOYOPw==gCUCy3PG8eL9KJ62
+
+import Foundation
+import Alamofire
+
+/**
+Se hace la peticion con el token de acceso
+Me base en el documento del laboratorio y notas que hice en Obsidian
+*/
+
+import Alamofire
+
+class CovidAPIService {
+    static let shared = CovidAPIService()
+
+    static let token = "tmEwAP5zJ4bllnMGJOYOPw==gCUCy3PG8eL9KJ62"
+    
+    func getCovidList() async -> [Result]? {
+        
+        let url = "\(Api.base)/canada"
+
+        let headers: HTTPHeaders = [
+            // "Authorization" : "Bearer \(token)",
+            "Authorization" : "Bearer \(CovidAPIService.token)",
+            "Accept": "application/json"
+        ]
+
+        let taskRequest = AF.request(url, method: .get, headers: headers).validate()
+        let response = await taskRequest.serializingData().response
+        
+        switch response.result {
+             case .success(let data):
+                 do {
+                     let response = try JSONDecoder().decode(Response.self, from: data)
+                     return response.results
+                 } catch {
+                     return nil
+                 }
+             case let .failure(error):
+                 debugPrint(error.localizedDescription)
+                 return nil
+             }
+    }
+}
